fe72d68b3cbac2fcbe3c6df23293b5e61764cf34
Daniel Robinson <dan.rob.xor@gmail.com>
2015-07-06 21:15:51 +0100

    add initial preffered language setting

--- src/client/component/langSelector/ctrl.js
+++ src/client/component/langSelector/ctrl.js
@@ -8,13 +8,16 @@
     return function(ngModule){
         ngModule
             .controller('langSelectorCtrl', [ '$scope', 'i18n', function($scope, i18n){
+                var prefLang = window.navigator.languages ? window.navigator.languages[0] : null;
+                prefLang = prefLang || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
                 $scope.langs = JSON.parse(langs);
                 $scope.change = function(){
                         if($scope.selected && $scope.selected.code)
                                 i18n.setLang($scope.selected.code);
                         else
-                            i18n.setLang(null);
+                            i18n.setLang(prefLang);
                     };
+                i18n.setLang(prefLang);
             }])
             .directive('cpLangSelector', function(){
                 return {restrict: 'E', template: tpl};
